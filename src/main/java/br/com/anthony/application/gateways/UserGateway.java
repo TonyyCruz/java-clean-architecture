e38fb7f1536ea1633a30b0fb1ead72c1ff0deafd
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-03-12 18:46:40 -0300

    feat: create UserGateway

--- src/main/java/br/com/anthony/application/gateways/UserGateway.java
+++ src/main/java/br/com/anthony/application/gateways/UserGateway.java
@@ -1,2 +1,7 @@
-package br.com.anthony.application.gateways;public class UserGateway {
+package br.com.anthony.application.gateways;
+
+import br.com.anthony.domain.entity.User;
+
+public interface UserGateway {
+    User createUser(User user);
 }
--- src/main/java/br/com/anthony/application/usercases/CreateUserIterator.java
+++ src/main/java/br/com/anthony/application/usercases/CreateUserIterator.java
@@ -1,4 +1,16 @@
 package br.com.anthony.application.usercases;
 
-public class CreateUserInteractor {
+import br.com.anthony.application.gateways.UserGateway;
+import br.com.anthony.domain.entity.User;
+
+public class CreateUserIterator {
+    final private UserGateway userGateway;
+
+    public CreateUserIterator(UserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
+    public User createUser(User user) {
+        return userGateway.createUser(user);
+    }
 }
